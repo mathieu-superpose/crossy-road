b3a18d54953e8d815518b0b5431917634eaff5a5
Mathieu Joly <superpose@minimac.local>
2025-03-22 10:21:41 +0100

    add Player Bounds

--- src/game/components/Player.tsx
+++ src/game/components/Player.tsx
@@ -1,9 +1,13 @@
+import { Bounds } from "@react-three/drei"
+
 function Player() {
   return (
-    <mesh position={[0, 0, 10]}>
-      <boxGeometry args={[15, 15, 20]} />
-      <meshLambertMaterial color={0xffffff} flatShading />
-    </mesh>
+    <Bounds fit clip observe margin={10}>
+      <mesh position={[0, 0, 10]}>
+        <boxGeometry args={[15, 15, 20]} />
+        <meshLambertMaterial color={0xffffff} flatShading />
+      </mesh>
+    </Bounds>
   )
 }
 
